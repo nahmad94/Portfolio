3cfc23c8f6a1a1d5b0c7c6d95ee3c37ea3feab85
nahmad94 <deevan11@gmail.com>
2023-02-16 01:58:21 -0500

    sets up homepage

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Resume from './Resume';
 import Projects from './Projects';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Resume />
       <Projects />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section id="welcome_head">
+        <div class="welcome">
+            <h1>Naveed Ahmad</h1>
+            <p>Business Developer, Engineer, Problem-Solver</p>
+        </div>     
+        <img id="my_pic"  src='./Pics/me.jpg' alt="Picture of Naveed" />   
+    </section>
+  )
+}
