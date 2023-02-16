f058c25eb33c775dcf7202dde93144fdb4c3ea75
nahmad94 <deevan11@gmail.com>
2023-02-16 01:48:34 -0500

    sets up projects

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Resume from './Resume';
+import Projects from './Projects';
 
 function App() {
   return (
     <div>
       <Header />
       <Resume />
+      <Projects />
     </div>
   );
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+        <h1>Sample Projects</h1>
+    
+        <a href="https://github.com/nahmad94" target="_blank">Check out my work!</a>
+
+        <div class="row">
+        <div class="column">
+            <img src='./Pics/me.jpg' alt="project 3" />
+        </div>
+        <div class="column">
+            <img src='./Pics/me.jpg' alt="project 4" />
+            <img src='./Pics/me.jpg' alt="project 2" />
+        </div>
+        <div class="column">
+            <img src='./Pics/me.jpg' alt="project 1" />
+        </div>
+        </div>
+    </div>
+  )
+}
