d1d6e582438ff451ee59101f0cf2b8dc16204ad0
nahmad94 <deevan11@gmail.com>
2023-03-10 12:59:15 -0500

    updates toggle on header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import Header from './Header';
 import Resume from './Resume';
 import Projects from './Projects';
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,33 +6,22 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+    <Navbar expand='med' fixed='top'>
+      <Container className='navbar' >
+        <Navbar.Brand>
+          <img src='./Pics/logo.png' />
+        </Navbar.Brand>
+        {/* <Navbar.Toggle className='toggle' aria-controls='basic-navbar-nav' /> */}
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Projects</Nav.Link>
+            <Nav.Link>Resume</Nav.Link>
+            <Nav.Link>Contact</Nav.Link>
+            <Nav.Link>Social</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        
       </Container>
     </Navbar>
   )
