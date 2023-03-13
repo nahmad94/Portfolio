80ea580383dd3e8b8da33e426d2605eab98ff068
nahmad94 <deevan11@gmail.com>
2023-03-12 20:37:16 -0400

    sets up main page

---
+++ src/Components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Home'
+import Projects from './Projects'
+import Resume from './Resume'
+import Contact from './Contact'
+
+export default function MainPage() {
+  return (
+    <div>
+        <section id='home'>
+            <Home />
+        </section>
+    </div>
+  )
+}
